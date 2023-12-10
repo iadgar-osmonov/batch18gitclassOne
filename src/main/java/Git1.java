1c3742176b37bea000597d9d5c0f3a8e6d238da4
iadgar-osmonov <iadgar.osmonov@gmail.com>
2023-12-10 13:21:13 -0600

    idea

--- src/main/java/Git1.java
+++ src/main/java/Git1.java
@@ -7,6 +7,7 @@ public class Git1 {
         //third change
         //change from branch number1
         //git change in branch number 1
+        System.out.println("Intellij");
 
 
     }
--- src/main/java/Git2.java
+++ src/main/java/Git2.java
@@ -1,6 +1,6 @@
 public class Git2 {
     public static void main(String[] args) {
-        // this is update from br2 and to show conflicts
+        // this is update from br2 and to show conflict
 
     }
 }
