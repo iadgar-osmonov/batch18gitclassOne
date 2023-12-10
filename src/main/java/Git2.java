b57468174266e584a4213c51b7f41d8d61b24a0a
iadgar-osmonov <iadgar.osmonov@gmail.com>
2023-12-10 13:25:52 -0600

    branch 2

--- src/main/java/Git2.java
+++ src/main/java/Git2.java
@@ -1,6 +1,7 @@
 public class Git2 {
     public static void main(String[] args) {
         // this is update from br2 and to show conflict
+        System.out.println("Git class 2 branch");
 
     }
 }
